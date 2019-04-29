3f4d8824b7331e77803e08960d229241e19ed4c8
Rahat Chowdhury <rahatchowdhury@Rahat-Chowdhurys-MacBook-Air.local>
2019-04-29 17:00:18 -0400

    update gatsby add hackefest post and google analytics

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import { graphql } from 'gatsby';
 
 class NotFoundPage extends React.Component {
   render() {
