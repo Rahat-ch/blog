4d5b0933fba47527a3ed755678accc5911686971
Rahat Chowdhury <rahatchowdhury@Rahat-Chowdhurys-MacBook-Air.local>
2019-04-29 16:19:03 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-139272695-1`,
       },
     },
     `gatsby-plugin-feed`,
